fa1cbebf01a062389f8dd60ae0c0116224c143de
YuhanLiin <linyuhan0315@hotmail.com>
2019-04-14 00:43:45 -0400

    Add sigmoid

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -1,10 +1,15 @@
 import numpy as np
 
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
 class LinearClassifier:
     def __init__(self, num_classes, num_features):
+        # let c = num_classes
         self.num_classes = num_classes
+        # let n = num_features
         self.num_features = num_features
-        # features * classes matrix
+        # c * n+1 matrix
         self.hypothesis = np.zeros((num_classes, num_features + 1))
     
     def predict(self, features):
@@ -12,5 +17,23 @@ class LinearClassifier:
         result = self.hypothesis @ features
         return np.argmax(result)
 
-    def train(self, data):
+    def cost(self, X, y):
+        # X -> m * n+1
+        assert X.shape[1] == self.num_features + 1
+        # y -> m * 1
+        assert y.shape == (X.shape[0], 1)
+        m = y.shape[0]
+
+        # y -> m * c
+        y = np.equal(np.repeat(np.arange(self.num_classes).reshape(1, -1), m, 0),
+                     np.repeat(y, self.num_classes, 1)).astype(int)
+        # h -> m * c
+        h = X @ self.hypothesis.T
+        print(y)
+        print(h)
+        # Logistic regression cost fn, produces 1 * c vector
+        cost = - np.sum(y * np.log(h) + (1 - y) * np.log(1 - h), 0) / m
+        print(cost)
+
+    def train(self, X, y, Xval, yval, Xtest, ytest):
         pass
--- tests/test_classifier.py
+++ tests/test_classifier.py
@@ -1,7 +1,28 @@
 import numpy as np
-from classifier.classifier import LinearClassifier
+from classifier.classifier import LinearClassifier, sigmoid
 
 def test_sanity_predict():
     classifier = LinearClassifier(num_classes=6, num_features=4)
     result = classifier.predict(np.array([1., 2., 3., 4.]))
     assert result == 0
+
+def test_sanity_cost():
+    classifier = LinearClassifier(num_classes=6, num_features=4)
+    X = np.array([[1, 2, 4, 3, 5],
+                  [1, 4, 5, 5, 10]])
+    y = np.array([3, 4]).reshape(-1, 1)
+    classifier.cost(X, y)
+
+def test_sigmoid():
+    sig1 = 1 / (1 + np.exp(-1))
+    sig2 = 1 / (1 + np.exp(-2))
+    assert sigmoid(0) == .5
+    assert sigmoid(1) == sig1
+    assert (
+        sigmoid(np.array([
+            [0, 1], 
+            [2, 0]])) ==
+        np.array([
+            [.5, sig1],
+            [sig2, .5]])
+    ).all()
