Fix feature scaling to use mean normalization

poweredarm/classifier.py
@@ -4,7 +4,7 @@ def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
 class LinearClassifier:
-    def __init__(self, num_classes, num_features, scaling_params=None, init_hyp=None):
+    def __init__(self, num_classes, num_features, offset, scaling_params=None, init_hyp=None):
         assert num_classes >= 2
 
         # let c = num_classes
@@ -15,6 +15,7 @@ class LinearClassifier:
         self.hypothesis = np.zeros((num_features + 1, num_classes), dtype=np.float32)
         # c length vector, defaults to all 1s (no feature scaling)
         self.scaling_params = np.ones(num_features)
+        self.offset = offset
 
         if init_hyp is not None:
             # Dependency injection for testing
@@ -75,12 +76,14 @@ class LinearClassifier:
         return np.array(costs)
 
     def scale_features(self, X):
-        return X / self.scaling_params[None, :]
+        return (X-self.offset[None, :]) / self.scaling_params[None, :]
 
     def train(self, X, y, **optimization_args):
         self._check_data_shape(X, y)
         # Add column of ones
         X = np.c_[ np.ones(X.shape[0]), self.scale_features(X) ]
+        print(np.mean(X[:, 1:], 0))
+        print(np.std(X[:, 1:], 0))
 
         # Run optimization function and return the costs for debugging
         costs = self.gradient_descent(lambda: self.cost_delta(X, y),

poweredarm/train_classifier.py
@@ -7,10 +7,10 @@ def train_linear_classifier(data_files):
     # 80-20 data split
     (X, y), (Xtest, ytest) = create_dataset(data, [0.8])
 
-    classifier = LinearClassifier(num_classes=4, num_features=8,
-                                  scaling_params=np.repeat(1600, 8))
+    classifier = LinearClassifier(num_classes=4, num_features=8, offset=np.mean(X, 0),
+                                  scaling_params=np.max(X, 0) - np.min(X, 0))
     
-    costs = classifier.train(X, y, rate=0.01, num_iter=500)
+    costs = classifier.train(X, y, rate=0.01, num_iter=1000)
     print(costs)
     acc, recalls = classifier.evaluate(Xtest, ytest)
     print(recalls)