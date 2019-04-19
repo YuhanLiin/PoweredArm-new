65cdcf85ab4c4a6228cb321cfc51983fb236bc08
YuhanLiin <linyuhan0315@hotmail.com>
2019-04-19 15:47:31 -0400

    Make training test run 5 times

--- poweredarm/train_classifier.py
+++ poweredarm/train_classifier.py
@@ -9,6 +9,6 @@ def train_linear_classifier(data_files):
 
     classifier = LinearClassifier(num_classes=4, num_features=8)
 
-    costs = classifier.train(X, y, rate=0.02, num_iter=1000)
+    costs = classifier.train(X, y, rate=0.02, num_iter=1200)
     acc, recalls = classifier.evaluate(Xtest, ytest)
     return acc, recalls
--- tests/test_train_data.py
+++ tests/test_train_data.py
@@ -4,7 +4,9 @@ import pytest
 from poweredarm.train_classifier import train_linear_classifier
 
 @pytest.mark.train
-def test_train_classifier_on_data():
+# Run multiple times, since each run uses different training/test data
+@pytest.mark.parametrize('execution_num', range(5))
+def test_train_classifier_on_data(execution_num):
     # Train classifier on actual data and expect high accuracy
     acc, recalls = train_linear_classifier(glob.glob('tests/data/*.csv'))
     assert acc > 0.992 and (recalls > 0.98).all()
