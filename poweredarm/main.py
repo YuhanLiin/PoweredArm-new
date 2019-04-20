86bf5b45fdfd5b0392d8b218ee8228e665502123
YuhanLiin <linyuhan0315@hotmail.com>
2019-04-19 20:47:06 -0400

    Add help text

--- poweredarm/main.py
+++ poweredarm/main.py
@@ -15,7 +15,7 @@ def print_results(acc, recalls):
         print('Class {} has recall of: {}'.format(cls, recall))
 
 if __name__ == '__main__':
-    command = sys.argv[1] 
+    command = sys.argv[1]
 
     if command == 'train':
         data_files = glob_args(2)
@@ -32,6 +32,29 @@ if __name__ == '__main__':
         if len(data_files) == 0:
             print("No data files detected for the 'evaluate' option")
             sys.exit(1)
-        
+
         acc, recalls = evaluate_linear_classifier(classifer_file, data_files)
         print_results(acc, recalls)
+
+    elif command == 'help':
+        print('Usage: python -m poweredarm.main [command] [..args]')
+        print('')
+
+        print('COMMAND train')
+        print('Usage: train [..data_files]')
+        print('Trains a classifier using the supplied CSV data files and saves it.')
+        print('')
+
+        print('COMMAND evaluate')
+        print('Usage: train classifier_file [..data_files]')
+        print('Loads a classifier from an existing file and evaluates ' +
+              'how accurately it predicts the supplied CSV dataset.')
+        print('')
+
+        print('COMMAND help')
+        print('Show this help message')
+        print('')
+
+    else:
+        print(f"Invalid command '{command}'. Use command 'help' to show all commands")
+        sys.exit(1)
