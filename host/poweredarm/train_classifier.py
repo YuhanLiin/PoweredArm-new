4bbe51295242441d7a2b1914bff9667887eb4009
YuhanLiin <linyuhan0315@hotmail.com>
2019-04-23 20:09:12 -0400

    Add visualize_realtionships

--- host/poweredarm/train_classifier.py
+++ host/poweredarm/train_classifier.py
@@ -5,10 +5,18 @@ from matplotlib import pyplot as plt
 from poweredarm.classifier import LinearClassifier
 from poweredarm.data_processing import (aggregate_csv, create_dataset)
 
+def visualize_relationships(X):
+    for i in range(8):
+        for j in range(i+1, 8):
+            plt.plot(X[:, i], X[:, j], 'b.')
+            plt.title('{} vs {}'.format(i, j))
+            plt.show()
+
 def evaluate_linear_classifier(classifier_file, data_files):
     classifier = LinearClassifier.load(classifier_file)
     data = aggregate_csv(data_files)
     X, y = create_dataset(data, [])
+    #visualize_relationships(X)
 
     acc, recalls = classifier.evaluate(X, y)
     return acc, recalls
