9b4ef4813182c27ebe0f1ce620dc88681c850b4b
YuhanLiin <linyuhan0315@hotmail.com>
2019-06-06 01:14:53 -0400

    Renamed hello to target and renamed esp project to poweredarm

--- target/tests/classifier.h
+++
@@ -1,7 +0,0 @@
-// Mock classifier.h file for tests
-
-#define CLASSIFIER_NUM_FEATURES 4
-#define CLASSIFIER_NUM_CLASSES 2
-#define CLASSIFIER_WEIGHT {0}
-#define CLASSIFIER_OFFSET {0}
-#define CLASSIFIER_DIVISOR {0}
